925a121fbb944b93ad344b9a497327eeedbcc743
Reid Holmes (MBA) <rtholmes@gmail.com>
2016-10-10 14:58:24 -0700

    add InsightFacade stubs for D2.

---
+++ src/controller/InsightFacade.ts
@@ -0,0 +1,11 @@
+/*
+ * This should be in the same namespace as your controllers
+ */
+
+import {IInsightFacade, InsightResponse} from "./IInsightFacade";
+
+export default class InsightFacade implements IInsightFacade {
+
+    // TODO: need to implement this
+
+}
--- test/DatasetControllerSpec.ts
+++ test/DatasetControllerSpec.ts
@@ -16,7 +16,7 @@ describe("DatasetController", function () {
     afterEach(function () {
     });
 
-    it("Should be able to receive a Dataset", function () {
+    it("Should be able to reject an invalid Dataset", function () {
         Log.test('Creating dataset');
         let content = {key: 'value'};
         let zip = new JSZip();
@@ -24,13 +24,14 @@ describe("DatasetController", function () {
         const opts = {
             compression: 'deflate', compressionOptions: {level: 2}, type: 'base64'
         };
+
         return zip.generateAsync(opts).then(function (data) {
             Log.test('Dataset created');
             let controller = new DatasetController();
             return controller.process('setA', data);
         }).then(function (result) {
             Log.test('Dataset processed; result: ' + result);
-            expect(result).to.equal(true);
+            expect(result).to.equal(false); // zip file was invalid, should not have passed
         });
 
     });
