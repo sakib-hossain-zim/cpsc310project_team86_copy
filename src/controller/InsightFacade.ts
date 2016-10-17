ac434319942943b91a639e5dbda41ff2922836e6
Harnoor <harnoor_909@hotmail.com>
2016-10-17 11:34:04 -0700

    stubs for insight

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,11 +1,18 @@
 /*
  * This should be in the same namespace as your controllers
  */
-
+import {QueryRequest} from "./QueryController";
 import {IInsightFacade, InsightResponse} from "./IInsightFacade";
 
 export default class InsightFacade implements IInsightFacade {
 
     // TODO: need to implement this
+    public addDataset (id:string, content: string) : any //Promise<InsightResponse>
+    {}
 
+    public removeDataset (id:string): any //Promise<InsightResponse>
+    {}
+
+    public performQuery (query: QueryRequest): any //Promise<InsightResponse>
+    {}
 }
