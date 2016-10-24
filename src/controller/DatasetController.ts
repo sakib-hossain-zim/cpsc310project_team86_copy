5233c703119f8660600078b5fd442566439457c2
janelle <janelle.sy@alumni.ubc.ca>
2016-10-24 02:23:31 -0700

    please fix 204 already

--- src/controller/DatasetController.ts
+++ src/controller/DatasetController.ts
@@ -63,6 +63,7 @@ export default class DatasetController {
         return that.datasets;
     }
 
+    public previous: boolean = false;
 
     /**
      * Process the dataset; save it to disk when complete.
@@ -76,6 +77,12 @@ export default class DatasetController {
 
         let that = this;
         let processedDataset : toBeAdded[] = [];
+
+        if (this.datasets[id] != null || fs.existsSync('./data/' + id + '.json')) {
+            this.previous = true;
+        } else {
+            this.previous = false;
+        }
 
         return new Promise(function (fulfill, reject) {
             try {
