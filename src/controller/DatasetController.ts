c5a1efc426f7c41744588a7b2c07cef4cf41da5b
Harnoor <harnoor_909@hotmail.com>
2016-11-10 10:09:10 -0800

    lat lon executes last --- wtf promises

--- src/controller/DatasetController.ts
+++ src/controller/DatasetController.ts
@@ -16,31 +16,6 @@ export interface Datasets {
     [id: string]: {};
 }
 
-// interface toBeAddedJson {
-//     courses_dept: string;
-//     courses_id: string;
-//     courses_avg: number;
-//     courses_instructor: string;
-//     courses_title: string;
-//     courses_pass: number;
-//     courses_fail: number;
-//     courses_uuid: string;
-//     courses_audit: number;
-// }
-//
-// interface toBeAddedHtml {
-//     rooms_fullname: string;
-//     rooms_shortname: string;
-//     rooms_number: string;
-//     rooms_name: string;
-//     rooms_address: string;
-//     rooms_lat: number;
-//     rooms_lon: number;
-//     rooms_seats: number;
-//     rooms_type: string;
-//     rooms_furniture: string;
-//     rooms_href: string;
-// }
 
 export default class DatasetController {
 
