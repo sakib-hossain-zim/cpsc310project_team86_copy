69625359db018940c11521ab8172040e0ceb82c2
SimmieG <Sp05_@hotmail.com>
2016-11-27 22:50:21 -0800

    revert back and try d3

--- src/controller/QueryController.ts
+++ src/controller/QueryController.ts
@@ -600,11 +600,11 @@ export default class QueryController {
                 ORretValues.push(dataObj["courses_uuid"]);
             }
         }
-        // if (id == 'rooms') {
-        //     for (let dataObj of data) {
-        //         ORretValues.push(dataObj["rooms_name"]);
-        //     }
-        // }
+        if (id == 'rooms') {
+            for (let dataObj of data) {
+                ORretValues.push(dataObj["rooms_name"]);
+            }
+        }
 
         if (field == 'AND') {
             for (let obj of queryData) {
@@ -644,19 +644,19 @@ export default class QueryController {
             }
             for (let retObj of ORReturnData) {
                 for (let value of ORretValues) {
-                   // if (id == "courses") {
+                    if (id == "courses") {
                         if (retObj["courses_uuid"] == value) {
                             ORReturnData2.push(retObj);
                             let index = ORretValues.indexOf(value);
                             ORretValues.splice(index, 1);
-                    //    }
-                    //  } else {
-                    //      if (retObj["rooms_name"] == value) {
-                    //          ORReturnData2.push(retObj);
-                    //          let index = ORretValues.indexOf(value);
-                    //          ORretValues.splice(index, 1);
-                    //      }
                         }
+                    } else {
+                        if (retObj["rooms_name"] == value) {
+                            ORReturnData2.push(retObj);
+                            let index = ORretValues.indexOf(value);
+                            ORretValues.splice(index, 1);
+                        }
+                    }
                 }
             }
             return ORReturnData2;
